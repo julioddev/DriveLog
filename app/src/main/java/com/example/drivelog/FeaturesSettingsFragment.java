c97eaa169f314567a17b1475014988d2d1a7f5ed
julioddev <julioddev@gmail.com>
2026-08-20 19:53:31 -0300

    Update FeaturesSettingsFragment.java

--- app/src/main/java/com/example/drivelog/FeaturesSettingsFragment.java
+++ app/src/main/java/com/example/drivelog/FeaturesSettingsFragment.java
@@ -2,6 +2,7 @@ package com.example.drivelog;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
