708b0fc6d0c88c8306b1cd8ef72090570b1bc6fb
julioddev <julioddev@gmail.com>
2026-08-24 15:57:49 -0300

    Update 2 240826

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.example.drivelog"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
+        versionCode = 7
         versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
